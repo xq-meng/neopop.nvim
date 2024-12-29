88f7456ac0d064c4b0b2a61a39ef05cfadeadaeb
xqmeng <xiangqiao.meng@connect.polyu.hk>
2024-12-29 14:54:57 +0800

    feat: add shortcut support

--- lua/neopop/init.lua
+++ lua/neopop/init.lua
@@ -3,26 +3,15 @@ local M = {}
 local telescope = require('telescope.builtin')
 local gitsigns = require('gitsigns')
 
-function M.choice(x)
-    -- LSP
-    if x == 'Reference' then
-        telescope.lsp_references()
-        -- vim.api.nvim_command("Telescope lsp_references")
-    elseif x == 'Definition' then
-        telescope.lsp_definitions()
-        -- vim.api.nvim_command("Telescope lsp_definitions")
-    elseif x == 'Implementation' then
-        telescope.lsp_implementations()
-        -- vim.api.nvim_command("Telescope lsp_implementations")
-    elseif x == 'Declaration' then
-        vim.lsp.buf.declaration()
-    -- Gitsign
-    elseif x == 'Preview Git Changes' then
-        gitsigns.preview_hunk()
-    elseif x == 'Reset Git Changes' then
-        gitsigns.reset_hunk()
-    end
-end
+local menu_items = {
+    {"Reference", "r", telescope.lsp_references},
+    {"Definition", "d", telescope.lsp_definitions},
+    {"Implementation", "i", telescope.lsp_implementations},
+    {"Declaration", "D", vim.lsp.buf.declaration},
+    {"--------------------", ""},
+    {"Preview Git Changes", "P", gitsigns.preview_hunk},
+    {"Reset Git Changes", "R", gitsigns.reset_hunk},
+}
 
 function M.show_menu()
     local cursor_word = vim.fn.expand('<cword>')
@@ -31,46 +20,67 @@ function M.show_menu()
         return
     end
 
-    local menu_items = {
-        {"Reference", ""},
-        {"Definition", ""},
-        {"Implementation", ""},
-        {"Declaration", ""},
-        {"--------------------", ""},
-        {"Preview Git Changes", ""},
-        {"Reset Git Changes", ""},
-    }
     local menu_content = {}
     for _, item in ipairs(menu_items) do
         -- table.insert(menu_content, item[1] .. " - " .. item[2])
-        table.insert(menu_content, item[1])
+        -- table.insert(menu_content, item[1])
+        if item[2] and item[2] ~= '' then
+            table.insert(menu_content, string.format("%-22s[%s]", item[1], item[2]))
+        else
+            table.insert(menu_content, string.format("%-22s", item[1]))
+        end
     end
 
     local buf = vim.api.nvim_create_buf(false, true)
     vim.api.nvim_buf_set_lines(buf, 0, -1, false, menu_content)
+    vim.api.nvim_buf_set_option(buf, 'modifiable', false)
 
     local win = vim.api.nvim_open_win(buf, true, {
         relative = 'cursor',
         row = 1,
         col = 1,
-        width = 20,
+        width = 26,
         height = #menu_content,
         style = 'minimal',
         border = 'rounded',
     })
 
+    -- cursor select.
     vim.api.nvim_buf_set_keymap(buf, 'n', '<CR>', '', {
         noremap = true,
         silent = true,
         callback = function()
             local cursor = vim.api.nvim_win_get_cursor(win)
-            local choice = menu_items[cursor[1]]
             vim.api.nvim_win_close(win, true)
-            M.choice(choice[1])
+            if menu_items[cursor[1]][3] then
+                menu_items[cursor[1]][3]()
+            end
         end
     })
 
+    -- shortcut.
+    for _, item in ipairs(menu_items) do
+        if item[2] and item[2] ~= '' and item[3] then
+            vim.api.nvim_buf_set_keymap(buf, 'n', item[2], '', {
+                noremap = true,
+                silent = true,
+                callback = function()
+                    vim.api.nvim_win_close(win, true)
+                    item[3]()
+                end
+            })
+        end
+    end
+
+    -- close menu.
     vim.api.nvim_buf_set_keymap(buf, 'n', '<Esc>', '', {
+        noremap = true,
+        silent = true,
+        callback = function()
+            vim.api.nvim_win_close(win, true)
+        end
+    })
+    vim.api.nvim_buf_set_keymap(buf, 'n', 'q', '', {
         noremap = true,
         silent = true,
         callback = function()
